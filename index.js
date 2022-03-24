146ffc8777aa9a1c6a0a4e2c4689c9beeafe8419
Iker Macaya <iker.macaya@biko2.com>
2022-03-24 15:43:58 +0100

    Reading data from tasks module

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ const {tasks} = require('./tasks')
 app.get('/', (req, res) => {
     res.render('index', {
         "title": "Tasks Manager",
-        "tasks" : [
-                { "task": "Making first node express steps"},
-                { "task": "Rendering something"},
-                { "task": "JS CSS HTML DUSTJS NODEJS"},
-                { "task": "Primerity"},
-                { "task": "El Corte Inglés"}
-            ]
+        tasks  
         })
 })
 
--- tasks.js
+++ tasks.js
@@ -4,7 +4,9 @@ const tasks =
         {"id":2, "task": "Rendering something"},
         {"id":3, "task": "JS CSS HTML DUSTJS NODEJS"},
         {"id":4, "task": "Primerity"},
-        {"id":5, "task": "El Corte Inglés"}
+        {"id":5, "task": "El Corte Inglés"},
+        {"id":6, "task": "FireFly"},
+        {"id":7, "task": "OmniStore"}
 ]
 
 module.exports = {tasks}
