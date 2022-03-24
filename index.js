2f9d98f2c5ba5e88c10a1d54b1a9388bb5b0d92a
Iker Macaya <iker.macaya@biko2.com>
2022-03-24 08:57:09 +0100

    Adding some tasks

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+const {tasks} = require('./tasks')
+
+app.get('/', (req, res) => {
+    res.json(tasks)
+})
+
+app.listen(5000, () => {
+    console.log('Server listening on port 5000000...');
+})
---
+++ tasks.js
@@ -0,0 +1,10 @@
+const tasks = 
+    [
+        {"id":1, "task": "Making first node express steps"},
+        {"id":2, "task": "Rendering something"},
+        {"id":3, "task": "JS CSS HTML DUSTJS NODEJS"},
+        {"id":4, "task": "Primerity"},
+        {"id":5, "task": "El Corte Inglés"}
+]
+
+module.exports = {tasks}
